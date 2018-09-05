ec322881506213840e4585bb1390f41def07593a
DentroApps <33389655+DentroApps@users.noreply.github.com>
2018-09-05 21:14:59 +0300

    added fragment classes and listeners

---
+++ app/src/main/java/ru/dentro/geekbrains/levelthreelessonone/presenter/FragmentSetter.java
@@ -0,0 +1,7 @@
+package ru.dentro.geekbrains.levelthreelessonone.presenter;
+
+import android.support.v4.app.Fragment;
+
+public interface FragmentSetter {
+    void setFragment (Fragment fragment);
+}
--- app/src/main/java/ru/dentro/geekbrains/levelthreelessonone/view/MainActivity.java
+++ app/src/main/java/ru/dentro/geekbrains/levelthreelessonone/view/MainActivity.java
@@ -1,14 +1,35 @@
 package ru.dentro.geekbrains.levelthreelessonone.view;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import ru.dentro.geekbrains.levelthreelessonone.R;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import ru.dentro.geekbrains.levelthreelessonone.R;
+import ru.dentro.geekbrains.levelthreelessonone.presenter.FragmentSetter;
+
+public class MainActivity extends AppCompatActivity implements
+                                        BottomNavigationView.OnNavigationItemSelectedListener,
+                                        FragmentSetter {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void setFragment(Fragment fragment) {
+
     }
 }
