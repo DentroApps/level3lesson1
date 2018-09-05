1aea1166e620257ed67aa1aa10bd5402cedb6999
DentroApps <33389655+DentroApps@users.noreply.github.com>
2018-09-05 20:39:14 +0300

    added visuals - container, bottom menu, menu items

---
+++ app/src/main/java/ru/dentro/geekbrains/levelthreelessonone/model/MyApp.java
@@ -0,0 +1,10 @@
+package ru.dentro.geekbrains.levelthreelessonone.model;
+
+import android.app.Application;
+
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/ru/dentro/geekbrains/levelthreelessonone/view/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.dentro.geekbrains.levelthreelessonone.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import ru.dentro.geekbrains.levelthreelessonone.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
